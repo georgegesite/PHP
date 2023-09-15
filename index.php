50623ffdcee51452eb10765f6a30cbebb9ed310a
George Gesite <georgesite33@gmail.com>
2023-09-15 10:13:23 +0800

    INTRO to PHP

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Udemy</title>
+</head>
+
+<body>
+    <?php
+    echo "This is my first PHP code";
+    $string = "this is a string";
+
+    ?>
+    <div>
+        <p>
+            <?php
+            echo date('y-m-d'); //built in date 
+            ?>
+        </p>
+    </div>
+    <div>
+        <p>
+            <?php
+
+            ?>
+        </p>
+    </div>
+</body>
+
+</html>
