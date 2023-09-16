c7e75d86e975bb9567548096eeff15cbc96560e6
George Gesite <georgesite33@gmail.com>
2023-09-16 09:50:51 +0800

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
     <?php
     echo "This is my first PHP code";
-    $string = "this is a string";
+
 
     ?>
     <div>
@@ -23,9 +23,115 @@
     <div>
         <p>
             <?php
-
+            $name = "George";
+            echo "Hello, $name. <br>";   // assign data types 
             ?>
         </p>
+    </div>
+    <div>
+        <?php
+        $num = 10; // variables are dynamically typed
+        echo $num;
+        ?>
+
+    </div>
+    <div>
+        <!-- String type in php -->
+        <?php
+        $greeting = "Hello, ";
+        $greeting = $greeting . "George <br>";
+        $welcoem = "Welcoome";
+        echo $greeting;
+
+        // heredoc example
+        $he = 'Bob';
+        $she = 'Alice';
+        $text = <<<TEXT
+        $he said "PHP is awesome".
+        "Of course" $she agreed."
+        TEXT;
+        echo $text;
+
+        $str = <<<IDENTIFIER
+        place a string here
+        it can span multiple lines
+        and include single quote ' and double quotes "
+        IDENTIFIER;
+
+        //Nowdoc example
+        $title = 'My site';
+
+        $header = <<<HEADER
+        <header>
+        <h1>$title</h1>
+        </header>
+        HEADER;
+        echo $header;
+
+        $str = <<<'IDENTIFIER'
+        place a string here
+        it can span multiple lines
+        and include single quote ' and double quotes "
+        IDENTIFIER;
+
+        $price = 20.00;
+        $tax = 0.07;
+        $total = $price + ($price * $tax);
+        echo "Total Price is \${$total}.";
+
+        $stringindex = "Hello world";
+        echo $stringindex[0];
+        echo $stringindex[6];
+
+        //single qoute escape character
+
+        echo '<br> it\'s a string';
+        //double qoute escape character
+        echo "<br> it's a \"hello\" ";
+
+        echo "<br>C:\\xamp\\htcods";
+
+        ?>
+    </div>
+
+    <div>
+        <?php
+        $number = 10; // numbers
+        $n_number = -20;
+        $binary = 0b111;
+        $octal = 0123;
+        $hexa = 0x1A;
+        echo $hexa;
+        echo "<br>";
+        echo $binary;
+        echo "<br>";
+        echo $octal;
+
+        ?>
+
+    </div>
+
+    <div>
+        <?php
+        //floats
+        ?>
+    </div>
+    <div>
+        <?php
+        //booleans\\
+        $myBoolean = true;
+        $anotherBoolean = false;
+        $number = 5;
+        $output = ($number == 5);
+        echo $output;
+
+        ?>
+    </div>
+    <div>
+        <?php
+        //next
+        $answer = true;
+        ?>
     </div>
 </body>
 
