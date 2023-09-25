64001fe44b91b7ec1372b4405876d15248e213da
George Gesite <georgesite33@gmail.com>
2023-09-25 11:02:49 +0800

    Create nullnumericstring.php

---
+++ nullnumericstring.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Null & NUmeric Strings</title>
+</head>
+
+<body>
+    <?php
+    $vari = null;
+    if (is_null($vari)) {
+        echo "variable is null";
+    }
+
+    echo "Hello World";
+    ?>
+</body>
+
+</html>
